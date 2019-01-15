fbfab496ee5ff6ec8b6e176c57383446d0704eea
lori <81104174@qq.com>
2019-01-15 21:28:05 +0800

    初始化

---
+++ src/main/java/com/lori/algorithmdemo/AlgorithmDemoApplication.java
@@ -0,0 +1,14 @@
+package com.lori.algorithmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmDemoApplication.class, args);
+	}
+
+}
+
