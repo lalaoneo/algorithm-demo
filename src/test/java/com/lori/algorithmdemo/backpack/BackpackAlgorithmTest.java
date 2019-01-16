523b63c06736eeb4702a249b9b4980207dd55f96
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 11:05:23 +0800

    Create BackpackAlgorithmTest.java

---
+++ src/test/java/com/lori/algorithmdemo/backpack/BackpackAlgorithmTest.java
@@ -0,0 +1,51 @@
+package com.lori.algorithmdemo.backpack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackpackAlgorithmTest {
+
+    @Test
+    public void testBackpackAlgorithm() {
+        List<Integer> locations = buildLocations();
+
+        Collections.sort(locations);
+
+        // 背包容量
+        int backpackCapacity = BackpackAlgorithm.getBackpackCapacity();
+
+        // 按步长划分的所有背包容量
+        List<Integer> allBackpackCapacity = BackpackAlgorithm.getAllBackpackCapacity(backpackCapacity);
+
+        // 初始化二维数组第一行第一列值,值都为0,设置动态规划的边界
+        int[][] value = BackpackAlgorithm.getMatchLocations(locations,allBackpackCapacity);
+
+        int i = value.length-1;
+        int j = value[locations.size()].length-1;
+        while (i>0&& j>0){
+            if (value[i][j] == 1){
+                System.out.println(locations.get(i-1));
+                j -= locations.get(i-1);
+            }
+            i--;
+        }
+    }
+
+    private List<Integer> buildLocations(){
+        List<Integer> locations = new ArrayList<>();
+        locations.add(5);
+        locations.add(6);
+        locations.add(2);
+        locations.add(2);
+        locations.add(4);
+        return locations;
+    }
+}
